405bd8ec00d62f35db907ca32c08b9f90ce65f94
okiogutu <okiogutu@gmail.com>
2024-04-07 06:46:35 +0000

    Test integration

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+"""This module tests clients"""
+
+import unittest
+from unittest.mock import (
+    Mock,
+    MagicMock,
+    PropertyMock,
+    patch
+)
+from typing import Dict
+from parameterized import parameterized, parameterized_class
+from requests import HTTPError
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+
+repo_payload = {
+    'repos_url': 'https://api.github.com/users/google/repos',
+    'repos': [
+        {
+            "id": 7697149,
+            "name": "episodes.dart",
+            "full_name": "google/episodes.dart",
+            "private": False,
+            "owner": {
+                "login": "google",
+                "id": 1342004,
+            },
+            "fork": False,
+            "url": "https://api.github.com/repos/google/episodes.dart",
+            "created_at": "2013-01-19T00:31:37Z",
+            "updated_at": "2019-09-23T11:53:58Z",
+            "has_issues": True,
+            "forks": 22,
+            "default_branch": "master",
+        },
+        {
+            "id": 7776515,
+            "node_id": "MDEwOlJlcG9zaXRvcnk3Nzc2NTE1",
+            "name": "cpp-netlib",
+            "private": False,
+            "owner": {
+                "login": "google",
+                "id": 1342004,
+            },
+            "fork": True,
+            "url": "https://api.github.com/repos/google/cpp-netlib",
+            "has_issues": False,
+            "forks": 59,
+            "default_branch": "master",
+        },
+    ]
+}
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Test case for `GithubOrgClient`"""
+    @parameterized.expand([
+        ('google', {'login': 'google'}),
+        ('abc', {'login': 'abc'}),
+    ])
+    @patch(
+        'client.get_json'
+    )
+    def test_org(self, org: str, res: Dict, mocked_func: MagicMock) -> None:
+        """Test org method"""
+        mocked_func.return_value = MagicMock(return_value=res)
+        gh_client = GithubOrgClient(org)
+        self.assertEqual(gh_client.org(), res)
+        mocked_func.assert_called_once_with(
+            f'https://api.github.com/orgs/{org}'
+        )
+
+    def test_public_repos_url(self) -> None:
+        """Test public repos"""
+        with patch(
+            'client.GithubOrgClient.org',
+            new_callable=PropertyMock
+        ) as prop_mock:
+            prop_mock.return_value = {
+                'repos_url': 'https://api.github.com/users/google/repos'
+            }
+            self.assertEqual(
+                GithubOrgClient('google')._public_repos_url,
+                'https://api.github.com/users/google/repos'
+            )
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json: MagicMock) -> None:
+        """Test the public repo"""
+        test_payload = repo_payload
+        mock_get_json.return_value = test_payload['repos']
+        with patch(
+            'client.GithubOrgClient._public_repos_url',
+            new_callable=PropertyMock
+        ) as res:
+            res.return_value = test_payload['repos_url']
+            self.assertEqual(
+                GithubOrgClient('google').public_repos(),
+                [
+                    'episodes.dart',
+                    'cpp-netlib'
+                ],
+            )
+            res.assert_called_once()
+        mock_get_json.assert_called_once()
+
+    @parameterized.expand([
+        ({'license': {'key': 'bsd-3-clause'}}, 'bsd-3-clause', True),
+        ({'license': {'key': 'bsl-1.0'}}, 'bsd-3-clause', False),
+    ])
+    def test_has_license(self, repo: Dict, key: str, expected: bool) -> None:
+        """Test the license method"""
+        gh_client = GithubOrgClient('google')
+        has_license = gh_client.has_license(repo, key)
+        self.assertEqual(has_license, expected)
+
+
+@parameterized_class([
+    {
+        'org_payload': TEST_PAYLOAD[0][0],
+        'repos_payload': TEST_PAYLOAD[0][1],
+        'expected_repos': TEST_PAYLOAD[0][2],
+        'apache2_repos': TEST_PAYLOAD[0][3],
+    }
+])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """Run integration tests for GithubOrgClient"""
+    @classmethod
+    def setUpClass(cls) -> None:
+        route_payload = {
+            'https://api.github.com/orgs/google': cls.org_payload,
+            'https://api.github.com/orgs/google/repos': cls.repos_payload,
+        }
+
+        def get_payload(url):
+            if url in route_payload:
+                return Mock(**{'json.return_value': route_payload[url]})
+            return HTTPError
+        cls.get_patcher = patch('requests.get', side_effect=get_payload)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """Test public repos"""
+        self.assertEqual(
+            GithubOrgClient('google').public_repos(),
+            self.expected_repos
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """Test public repos api with a license"""
+        self.assertEqual(
+            GithubOrgClient('google').public_repos(license='apache-2.0'),
+            self.apache2_repos
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Clean up"""
+        cls.get_patcher.stop()
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""This module tests nested maps"""
+import unittest
+from parameterized import parameterized
+from unittest.mock import Mock, patch
+from typing import (
+    Dict,
+    Mapping,
+    Sequence,
+    Union
+)
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Test case for utils.access"""
+    @parameterized.expand([
+        ({'a': 1}, ('a'), 1),
+        ({'a': {'b': 2}}, ('a'), {'b': 2}),
+        ({'a': {'b': 2}}, ('a', 'b'), 2),
+    ])
+    def test_access_nested_map(
+        self,
+        nested_map: Mapping,
+        path: Sequence,
+        expected: Union[Dict, int]
+    ) -> None:
+        """Test access_nested_map output"""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ('b',), KeyError),
+        ({'b': 4}, ('d', 'c'), KeyError),
+    ])
+    def test_access_nested_map_exception(
+        self,
+        nested_map: Mapping,
+        path: Sequence,
+        exception: Exception
+    ) -> None:
+        """Test access_nested_map exception"""
+        # NOTE: Ignore the linter warning it still passes
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Test case for get_json"""
+    @parameterized.expand([
+        ('http://example.com', {'payload': True}),
+        ('http://holberton.io', {'payload': False}),
+    ])
+    def test_get_json(
+        self,
+        test_url: str,
+        test_payload: Dict,
+    ) -> None:
+        """Test get_json output"""
+        attr = {'json.return_value': test_payload}
+        # NOTE: Ignore linter warning still passes
+        with patch('requests.get', return_value=Mock(**attr)) as req:
+            self.assertEqual(get_json(test_url), test_payload)
+            req.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test case to memoize"""
+
+    def test_memoize(self) -> None:
+        """Test `memoize` output"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(
+                TestClass,
+                "a_method",
+                return_value=lambda: 42,
+        ) as memo_func:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            memo_func.assert_called_once()
